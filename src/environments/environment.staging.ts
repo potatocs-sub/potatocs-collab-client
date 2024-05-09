eff05e4873eb3cd11f8500dcd1355ca9a34d742f
ljwoon <ljwoon@nsmarts.co.kr>
2024-05-09 09:58:28 +0900

    first commit

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -2,7 +2,8 @@ export const environment = {
   production: false,
   tokenName: 'coop',
   eCodeName: 'findPw',
-  apiUrl: 'http://localhost:3000/api/v1',
+  apiUrl: 'https://test-potatocs.com/api/v1',
   localhostUrl: 'localhost:3000',
-  domainUrl: 'localhost:3000',
+  domainUrl: 'https://test-potatocs.com',
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,8 @@
-export const environment = {};
+export const environment = {
+  production: true,
+  tokenName: 'coop',
+  eCodeName: 'findPw',
+  apiUrl: 'https://potatocs.com/api/v1',
+  localhostUrl: 'localhost:3000',
+  domainUrl: 'https://potatocs.com',
+};
