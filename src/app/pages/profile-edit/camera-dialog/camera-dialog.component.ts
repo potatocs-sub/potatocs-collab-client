cdfb91cde2df25c3aef966481a88233ddf43fe79
BAEKSEONGYUN <baek@nsmarts.co.kr>
2024-07-11 15:43:30 +0900

    camera test

--- src/app/pages/profile-edit/camera-dialog/camera-dialog.component.ts
+++ src/app/pages/profile-edit/camera-dialog/camera-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-camera-dialog',
@@ -8,6 +8,33 @@ import { Component } from '@angular/core';
     styleUrl: './camera-dialog.component.scss'
 })
 export class CameraDialogComponent {
+    @ViewChild('video') video: ElementRef<HTMLVideoElement>;
+    @ViewChild('canvas') canvas: ElementRef<HTMLCanvasElement>;
+
+    constructor() { }
+
+    ngOnInit() {
+        this.startCamera();
+    }
 
 
+    startCamera() {
+        navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+            this.video.nativeElement.srcObject = stream;
+            this.video.nativeElement.play();
+        });
+    }
+
+    captureFrame() {
+        const context = this.canvas.nativeElement.getContext('2d');
+        context.drawImage(this.video.nativeElement, 0, 0, 640, 480);
+        const frame = this.canvas.nativeElement.toDataURL('image/jpeg');
+        // Send the frame to the server
+        this.sendFrameToServer(frame);
+    }
+
+    sendFrameToServer(frame: string) {
+        // Implement the logic to send the frame to the server
+        // For example, using HttpClient to POST the frame to a server endpoint
+    }
 }
