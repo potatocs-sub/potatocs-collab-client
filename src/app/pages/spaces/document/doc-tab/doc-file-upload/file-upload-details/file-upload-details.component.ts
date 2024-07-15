95965f5d623c18ef9ac0deeba6dee1ad86fac290
park9379 <109051896+park9379@users.noreply.github.com>
2024-07-15 09:55:32 +0900

    스타일

--- src/app/pages/spaces/document/doc-tab/doc-file-upload/file-upload-details/file-upload-details.component.ts
+++ src/app/pages/spaces/document/doc-tab/doc-file-upload/file-upload-details/file-upload-details.component.ts
@@ -1,12 +1,13 @@
 import { Component, Inject, OnInit } from "@angular/core";
 import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { MaterialsModule } from "../../../../../../materials/materials.module";
 
 @Component({
 	selector: "app-file-upload-details",
 	templateUrl: "./file-upload-details.component.html",
 	styleUrls: ["./file-upload-details.component.scss"],
 	standalone: true,
-	imports: [],
+	imports: [MaterialsModule],
 })
 export class FileUploadDetailsComponent implements OnInit {
 	constructor(
