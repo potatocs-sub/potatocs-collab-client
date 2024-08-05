c4d1a3a838ddcb866aafb09629dd784d4994ccd6
BAEKSEONGYUN <baek@nsmarts.co.kr>
2024-08-05 17:12:31 +0900

    space 생성시 옵션 추가, 메뉴에 얼굴 표시

--- src/app/components/layout/side-nav/side-nav.component.ts
+++ src/app/components/layout/side-nav/side-nav.component.ts
@@ -12,52 +12,53 @@ import { SpacesService } from "../../../services/spaces/spaces.service";
 import { SpaceListStorageService } from "../../../stores/space-list-storage.service";
 
 @Component({
-	selector: "app-side-nav",
-	standalone: true,
-	imports: [MaterialsModule, CommonModule, SideNavItemComponent, RouterModule],
-	templateUrl: "./side-nav.component.html",
-	styleUrl: "./side-nav.component.scss",
+    selector: "app-side-nav",
+    standalone: true,
+    imports: [MaterialsModule, CommonModule, SideNavItemComponent, RouterModule],
+    templateUrl: "./side-nav.component.html",
+    styleUrl: "./side-nav.component.scss",
 })
 export class SideNavComponent {
-	navItems: any[] = sidenavRouteInfo;
+    navItems: any[] = sidenavRouteInfo;
 
-	// 의존성 주입
-	profilesService = inject(ProfilesService);
-	spacesService = inject(SpacesService);
+    // 의존성 주입
+    profilesService = inject(ProfilesService);
+    spacesService = inject(SpacesService);
 
-	userProfileInfo: WritableSignal<any> = this.profilesService.userProfileInfo;
-	userCompanyInfo: WritableSignal<any> = this.profilesService.userCompanyInfo;
-	space: WritableSignal<any> = this.spaceListStorageService.space;
+    userProfileInfo: WritableSignal<any> = this.profilesService.userProfileInfo;
+    userCompanyInfo: WritableSignal<any> = this.profilesService.userCompanyInfo;
+    space: WritableSignal<any> = this.spaceListStorageService.space;
 
-	constructor(
-		private sideNavService: SideNavService,
-		private navigationService: NavigationService,
-		private spaceListStorageService: SpaceListStorageService
-	) {
-		effect(() => {
-			console.log(this.space());
-		});
-	}
-	ngOnInit(): void {
-		this.sideNavService.updateSideMenu().subscribe(
-			(data: any) => {
-				const space = data.navList[0].spaces;
-				this.navItems[1].children[1].children = [];
-				for (let index = 0; index < space.length; index++) {
-					const element = {
-						type: "link",
-						label: space[index].displayName,
-						route: "space/" + space[index]._id,
-						isManager: false,
-						isReplacementDay: false,
-					};
-					this.navItems[1].children[1].children.push(element);
-				}
-				this.spaceListStorageService.space.set(this.navItems);
-			},
-			(err: any) => {
-				console.log("sideNavService error", err);
-			}
-		);
-	}
+    constructor(
+        private sideNavService: SideNavService,
+        private navigationService: NavigationService,
+        private spaceListStorageService: SpaceListStorageService
+    ) {
+        effect(() => {
+            console.log(this.space());
+        });
+    }
+    ngOnInit(): void {
+        this.sideNavService.updateSideMenu().subscribe(
+            (data: any) => {
+                const space = data.navList[0].spaces;
+                this.navItems[1].children[1].children = [];
+                for (let index = 0; index < space.length; index++) {
+                    const element = {
+                        type: "link",
+                        label: space[index].displayName,
+                        route: "space/" + space[index]._id,
+                        isManager: false,
+                        isReplacementDay: false,
+                        faceAuthentication: 'faceAuthentication' in space[index] ? space[index].faceAuthentication : false
+                    };
+                    this.navItems[1].children[1].children.push(element);
+                }
+                this.spaceListStorageService.space.set(this.navItems);
+            },
+            (err: any) => {
+                console.log("sideNavService error", err);
+            }
+        );
+    }
 }
