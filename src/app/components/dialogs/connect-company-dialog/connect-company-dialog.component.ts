25010ad557c941ed5d706628978d6964a46e533f
nsmarts-jmh <mcho@nsmarts.co.kr>
2024-08-19 09:18:49 +0900

    [fix]

--- src/app/components/dialogs/connect-company-dialog/connect-company-dialog.component.ts
+++ src/app/components/dialogs/connect-company-dialog/connect-company-dialog.component.ts
@@ -3,67 +3,59 @@ import { CommonModule } from '@angular/common';
 import { Component, Inject, inject } from '@angular/core';
 import { MaterialsModule } from '../../../materials/materials.module';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CompaniesService } from '../../../services/companies/companies.service';
 
 @Component({
   selector: 'app-connect-company-dialog',
   standalone: true,
-  imports: [
-    CommonModule,
-    MaterialsModule
-  ],
+  imports: [CommonModule, MaterialsModule],
   templateUrl: './connect-company-dialog.component.html',
-  styleUrl: './connect-company-dialog.component.scss'
+  styleUrl: './connect-company-dialog.component.scss',
 })
 export class ConnectCompanyDialogComponent {
-
   public dialogRef = inject(MatDialogRef<ConnectCompanyDialogComponent>);
-  public data: any = Inject(MAT_DIALOG_DATA)
-  addCodeInput = new FormControl('');
-  addCode = ''; // 검색어.
-  addBtn: boolean = false;
+  dialogsService = inject(DialogService);
+  companiesService = inject(CompaniesService);
+  public data: any = Inject(MAT_DIALOG_DATA);
+  fb = inject(FormBuilder);
 
-  dialogsService = inject(DialogService)
-  companiesService = inject(CompaniesService)
+  connectCompanyForm: FormGroup = this.fb.group({
+    companyCode: ['', [Validators.required]],
+  });
 
   requestCompanyConnection() {
     const company_code = {
-      company_code: this.addCodeInput.value,
+      company_code: this.connectCompanyForm.value.companyCode,
     };
-    this.dialogsService.openDialogConfirm('Do you want to register a company?').subscribe(result => {
-      if (result) {
-        this.companiesService.requestCompanyConnection(company_code).subscribe(
-          {
-            next: (value: any) => {
-              this.dialogRef.close();
-              this.dialogsService.openDialogPositive('Successfully, the process has done!');
-            },
-            error: (err: any) => {
-              if (err.error.message == '4') {
-                this.dialogsService.openDialogNegative('Already has a pending request.');
-                // alert('Already has a pending request.');
-              } else if (err.error.message == '5') {
-                this.dialogsService.openDialogNegative('Wrong code. Try again.');
-                // alert('Wrong code. Try again.');
-              }
-              this.addCodeInput.setValue('');
-            }
-          }
 
-        );
-      }
-    });
-  }
-  getRequestCompanyConnectionStatus() {
-    this.companiesService.getRequestCompanyConnectionStatus().subscribe({
-      next: (data: any) => {
-
-      },
-      error: (err: any) => {
-        console.log(err);
-      },
-    }
-    );
+    this.dialogsService
+      .openDialogConfirm('Do you want to register a company?')
+      .subscribe((result) => {
+        if (result) {
+          this.companiesService
+            .requestCompanyConnection(company_code)
+            .subscribe({
+              next: (value: any) => {
+                this.dialogRef.close();
+                this.dialogsService.openDialogPositive(
+                  'Successfully, the process has done!'
+                );
+              },
+              error: (err: any) => {
+                if (err.error.message == '4') {
+                  this.dialogsService.openDialogNegative(
+                    'Already has a pending request.'
+                  );
+                } else if (err.error.message == '5') {
+                  this.dialogsService.openDialogNegative(
+                    'Wrong code. Try again.'
+                  );
+                }
+                this.connectCompanyForm.value.companyCode.setValue('');
+              },
+            });
+        }
+      });
   }
 }
