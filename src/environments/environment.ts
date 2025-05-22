2b753cda664077ee23eb94f007d18d5bc03b5182
limhokyun <lim@nsmarts.co.kr>
2025-05-22 10:57:35 +0900

    [chore] 작업 시작 전 저장장

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,20 @@
+// export const environment = {
+// 	production: true,
+// 	tokenName: "coop",
+// 	eCodeName: "findPw",
+// 	apiUrl: "https://test-potatocs-lb.com/api/v1",
+// 	localhostUrl: "localhost:3300",
+// 	chatApiUrl: "https://test-potatocs-lb.com/chatApi/v1",
+// 	domainUrl: "https://test-potatocs-lb.com",
+// 	MEETING_FRONT_URL: "https://test-potatocs-lb.com",
+// };
 export const environment = {
 	production: true,
 	tokenName: "coop",
 	eCodeName: "findPw",
-	apiUrl: "https://test-potatocs-lb.com/api/v1",
+	apiUrl: "https://potatocs.com/api/v1",
 	localhostUrl: "localhost:3300",
-	chatApiUrl: "https://test-potatocs-lb.com/chatApi/v1",
-	domainUrl: "https://test-potatocs-lb.com",
-	MEETING_FRONT_URL: "https://test-potatocs-lb.com",
+	chatApiUrl: "https://potatocs.com/chatApi/v1",
+	domainUrl: "https://potatocs.com",
+	MEETING_FRONT_URL: "https://potatocs.com",
 };
